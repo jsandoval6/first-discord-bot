8c11bdd898653237cb4a1d3f0996dbd20e99833a
David Sandoval <davidsandoval281994@gmail.com>
2023-04-09 16:04:36 -0600

    added options and choices to / commands

--- bot.js
+++ bot.js
@@ -21,11 +21,16 @@ client.on("messageCreate", (msg) => {
 });
 
 client.on("interactionCreate", async (interaction) => {
-  console.log(interaction);
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === "ping") {
     await interaction.reply("Pong!");
+  }
+
+  if (interaction.commandName === "add") {
+    const num1 = interaction.options.get("first-number")?.value;
+    const num2 = interaction.options.get("second-number")?.value;
+    interaction.reply(`the sum is ${num1 + num2}`);
   }
 });
 
--- registration.js
+++ registration.js
@@ -1,10 +1,42 @@
 require("dotenv").config();
-const { REST, Routes } = require("discord.js");
+const { REST, Routes, ApplicationCommandOptionType } = require("discord.js");
 
 const commands = [
   {
     name: "ping",
     description: "Replies with Pong!",
+  },
+  {
+    name: "add",
+    description: "Add two numbers",
+    options: [
+      {
+        name: "first-number",
+        description: "first number",
+        type: ApplicationCommandOptionType.Number,
+        choices: [
+          {
+            name: "one",
+            value: 1,
+          },
+          {
+            name: "two",
+            value: 2,
+          },
+          {
+            name: "three",
+            value: 3,
+          },
+        ],
+        required: true,
+      },
+      {
+        name: "second-number",
+        description: "second number",
+        type: ApplicationCommandOptionType.Number,
+        required: true,
+      },
+    ],
   },
 ];
 
@@ -13,7 +45,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
   try {
     console.log("Started refreshing application (/) commands.");
 
-    await rest.put(Routes.applicationCommands("1094390797715120168"), {
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
       body: commands,
     });
 
